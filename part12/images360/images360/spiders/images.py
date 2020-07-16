78000d5fe15b68ad04d2b43ac7ba0a1cb3647881
lilihuakai <1356553590@qq.com>
2020-07-16 14:52:11 +0800

    Item Pipeline学习

---
+++ part12/images360/images360/spiders/images.py
@@ -0,0 +1,31 @@
+import scrapy
+import json
+from urllib.parse import urlencode
+from scrapy import Spider, Request
+from images360.items import Images360Item
+
+
+class ImagesSpider(scrapy.Spider):
+    name = 'images'
+    allowed_domains = ['image.so.com']
+    start_urls = ['http://image.so.com/']
+
+    def start_requests(self):
+        data = {'ch': 'photography', 'listtype': 'new'}
+        base_url = 'https://image.so.com/zjl?'
+        for page in range(1, self.settings.get('MAX_PAGE') + 1):
+            data['sn'] = page * 30
+            params = urlencode(data)
+            url = base_url + params
+            yield Request(url, self.parse)
+
+    def parse(self, response):
+        result = json.loads(response.text)
+        for image in result.get('list'):
+            item = Images360Item()
+            item['id'] = image.get('id')
+            item['url'] = image.get('qhimg_url')
+            item['title'] = image.get('title')
+            item['thumb'] = image.get('qhimg_thumb')
+            print('item', item)
+            yield item
